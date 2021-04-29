b1bc4c6a80793d3c5f5774a5046c300393d2ddad
Spencer Graff <948056+s-graff@users.noreply.github.com>
2021-04-28 22:53:03 -0600

    Completed bootcamp section 12 (lessons 132-141)

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func billTextChanged(_ sender: UITextField) {
+        
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(sender.value)
+        billTextField.endEditing(true)
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        billTextField.endEditing(true)
+    }
+
+
+}
+
