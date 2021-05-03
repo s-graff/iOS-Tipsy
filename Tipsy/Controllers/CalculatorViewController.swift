27e0aa88b1a57716fd56e64f6014ff7684baf30a
Spencer Graff <948056+s-graff@users.noreply.github.com>
2021-05-03 23:41:03 +0000

    Implement controls in calculator view

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    let NUM_PATTERN = try? NSRegularExpression(pattern: "(?:[-]\\s*+)?+\\d*+(?:[.]\\d++)?+")
+    var bill = 0.0
+    var tip = 0.1
+    var split = 2
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    @IBAction func billTextChanged(_ sender: UITextField) {
-        
+    func updateBill() {
+        let billText = billTextField.text
+        if(billText?.isEmpty ?? true) { return }
+        bill = Double(billText!) ?? bill
     }
+    
+    @IBAction func billTextChanged(_ sender: UITextField) {
+        updateBill()
+    }
+    
     @IBAction func tipChanged(_ sender: UIButton) {
         billTextField.endEditing(true)
         
@@ -28,16 +40,24 @@ class CalculatorViewController: UIViewController {
         
         sender.isSelected = true
         
-        
+        var str = sender.currentTitle ?? "0"
+        var range = NSRange(location: 0, length: str.utf16.count)
+        range = NUM_PATTERN?.rangeOfFirstMatch(in: str, range: range) ?? range
+        let match = String(str[Range(range, in: str)!])
+        if(match.utf16.count > 0){ str = match }
+        tip = Double(str) ?? tip
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        splitNumberLabel.text = String(sender.value)
         billTextField.endEditing(true)
+        split = Int(sender.value)
+        splitNumberLabel.text = String(split)
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
         billTextField.endEditing(true)
+        updateBill()
     }
-
 
 }
 
